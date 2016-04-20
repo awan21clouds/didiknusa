a0ad7a1c0ff7fbb5127e9cb9a081c5e96b86026b
awan21clouds <awan@21clouds.com>
2016-04-20 16:28:11 +0700

    config

---
+++ app/Confirmation.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Confirmation extends Model
+{
+    protected $table = 'confirmation';
+    public $timestamps = false;
+}
---
+++ app/Member.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    protected $table = 'member';
+    public $timestamps = false;
+
+    public function getRowsByUser($i){
+        $scholarship = DB::table('member')
+            ->select(DB::raw('ifnull((select count(scholarship_id) from scholarship where scholarship.member_id = member.member_id),0) as scholarship_count'), DB::raw('ifnull((select count(donation_id) from donation join transaction on transaction.transaction_id = donation.transaction_id where transaction.member_id = member.member_id) ,0) donation_count'), DB::raw('ifnull((select sum(total) from credit join transaction on transaction.transaction_id = credit.transaction_id where transaction.member_id = member.member_id),0) as credit_count'))
+            ->groupBy('member.member_id')
+            ->where('member.member_id', '=', $i)
+            ->first();
+        return $scholarship;
+    }
+}
