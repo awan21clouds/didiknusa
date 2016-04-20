e1a5ea727025c772e666ded3e48fa7ec779e1a06
awan21clouds <awan@21clouds.com>
2016-04-20 21:42:52 +0700

    config

--- resources/views/scholarship-detail.blade.php
+++ resources/views/scholarship-detail.blade.php
@@ -153,6 +153,24 @@
                                     dari target <span class="scholarship-currency">{{$scholarship->scholarship_target}}</span>
                                 </h4>
                             </div>
+                            <div class="box-footer text-black">
+                                <div class="row">
+                                    <div class="col-sm-12">
+                                        <!-- Progress bars -->
+                                        <div class="clearfix">
+                                            <span class="pull-left">Terkumpul</span>
+                                            <small class="pull-right scholarship-currency">{{ $scholarship->donation_total }}</small>
+                                        </div>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-green" style="width: {{ round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2).'%' }};">{{ round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2) }}%</div>
+                                        </div>
+                                        <div class="clearfix">
+                                            <small class="pull-left">@if(Session::has('member'))<a href="javascript:;" onclick="scholarshipDonation(this);" class="btn btn-success btn-sm" alt="{{$scholarship->scholarship_id}}" @if(round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2)>=100) disabled @endif>Berikan Beasiswa</a>@endif</small>
+                                            <small class="pull-right"><i class="fa fa-clock-o"></i> <span data-livestamp="{{ $scholarship->created }}"></span></small>
+                                        </div>
+                                    </div><!-- /.col -->
+                                </div><!-- /.row -->
+                            </div>
                             {{--<img class="img-thumbnail img-circle" src="{{ url('/') }}/public/adminLTE/dist/img/avatar5.png" alt="user image" style="display: block; margin: 0 auto;">--}}
                             <div class="box-tools">
                                 <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
@@ -166,6 +184,16 @@
                                 </dl>
                             </div>
                         </div>
+                        <div class="box-body no-padding bg-black">
+                            <div class="col-md-12">
+                                <div id="DateCountdown" class="col-sm-11" data-date="{{$scholarship->deadline}} 00:00:00" style="height: 100px; margin: 0px auto; margin-top:25px;"></div><!-- /.col -->
+                            </div>
+                        </div><!-- /.box-body -->
+                        <div class="box-body no-padding bg-green">
+                            <!--The calendar -->
+                            <div id="calendar" alt="{{$scholarship->scholarship_deadline}}" style="width: 100%"></div>
+                        </div><!-- /.box-body -->
+
                         @if(count($funders))
                             <div class="box-body no-padding">
                                 <ul class="users-list clearfix">
@@ -179,33 +207,6 @@
                                 </ul><!-- /.users-list -->
                             </div><!-- /.box-body -->
                         @endif
-                        <div class="box-body no-padding bg-black">
-                            <div class="col-md-12">
-                                <div id="DateCountdown" class="col-sm-11" data-date="{{$scholarship->deadline}} 00:00:00" style="height: 100px; margin: 0px auto; margin-top:25px;"></div><!-- /.col -->
-                            </div>
-                        </div><!-- /.box-body -->
-                        <div class="box-body no-padding bg-green">
-                            <!--The calendar -->
-                            <div id="calendar" alt="{{$scholarship->scholarship_deadline}}" style="width: 100%"></div>
-                        </div><!-- /.box-body -->
-                        <div class="box-footer text-black">
-                            <div class="row">
-                                <div class="col-sm-12">
-                                    <!-- Progress bars -->
-                                    <div class="clearfix">
-                                        <span class="pull-left">Terkumpul</span>
-                                        <small class="pull-right scholarship-currency">{{ $scholarship->donation_total }}</small>
-                                    </div>
-                                    <div class="progress">
-                                        <div class="progress-bar progress-bar-green" style="width: {{ round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2).'%' }};">{{ round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2) }}%</div>
-                                    </div>
-                                    <div class="clearfix">
-                                        <small class="pull-left">@if(Session::has('member'))<a href="javascript:;" onclick="scholarshipDonation(this);" class="btn btn-success btn-sm" alt="{{$scholarship->scholarship_id}}" @if(round((($scholarship->donation_total/$scholarship->scholarship_target)*100),2)>=100) disabled @endif>Berikan Beasiswa</a>@endif</small>
-                                        <small class="pull-right"><i class="fa fa-clock-o"></i> <span data-livestamp="{{ $scholarship->created }}"></span></small>
-                                    </div>
-                                </div><!-- /.col -->
-                            </div><!-- /.row -->
-                        </div>
                     </div>
                 </div>
             </div>
