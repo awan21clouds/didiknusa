1804b0d7c576972d9999ad950536835b9fb388db
awan21clouds <awan@21clouds.com>
2016-04-21 13:38:46 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -11,18 +11,18 @@
                 <!-- User name -->
                 <div class="lockscreen-name text-center">Segera lakukan konfirmasi pengiriman dana donasi anda.</div>
                 <div class="lockscreen-name text-center">Donasi yang perlu anda kirimkan sebesar : </div>
+                <br>
                 <div class="lockscreen-logo">
                     <a href="javascript:;"><b class="scholarship-currency">100000000</b></a>
                 </div>
                 <div class="help-block text-center">
-                    Pastikan anda telah mengirimkan dan melakukan konfirmasi sebelum batas waktu pengumpulan dana
+                    Pastikan anda telah mengirimkan dan melakukan konfirmasi sebelum batas waktu pengumpulan dana.
                 </div>
                 <div class="text-center">
                     <a href="{{ url('/') }}/home" class="animsition-link">Masuk Lagi</a>
                 </div>
                 <div class="lockscreen-footer text-center">
                     <h3><b>DidikBangsa</b>.com</h3>
-                    <br>
                     Copyright &copy; 2016<br>
                 </div>
             </div><!-- /.center -->
