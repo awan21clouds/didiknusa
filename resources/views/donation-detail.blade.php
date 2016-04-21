bb592e66c02b306fc370237646ea7b0a97fc4385
awan21clouds <awan@21clouds.com>
2016-04-21 12:52:59 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -1,17 +1,1 @@
-@extends('layout.master')
-
-@section('content')
-    <div class="container">
-        <!-- Main content -->
-        <section class="content">
-            A
-            @if(!Session::has('member'))
-                @include('layout.login')
-            @endif
-        </section><!-- /.content -->
-    </div><!-- /.container -->
-    <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <script src="../adminLTE/dist/js/home.js"></script>
-    <script src="../adminLTE/dist/js/scholarship_detail.js"></script>
-    @include('layout.script')
-@endsection
+Hello World!
