c18e394c0c4accf8658434859bb4ec92a3f277ba
awan21clouds <awan@21clouds.com>
2016-04-21 13:25:02 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -12,10 +12,10 @@
                 <div class="lockscreen-name text-center">Segera lakukan konfirmasi pengiriman dana donasi anda.</div>
                 <div class="lockscreen-name text-center">Donasi yang perlu anda kirimkan sebesar : </div>
                 <div class="lockscreen-logo">
-                    <a href="javascript:;"><b>Donasi Berhasil!</b></a>
+                    <a href="javascript:;"><b class="scholarship-currency">100000000</b></a>
                 </div>
                 <div class="help-block text-center">
-                    Pastikan anda telah mengirimkan dan melakukan konfirmasi sebelum batas waktu pengumpulan dan
+                    Pastikan anda telah mengirimkan dan melakukan konfirmasi sebelum batas waktu pengumpulan dana
                 </div>
                 <div class="text-center">
                     <a href="{{ url('/') }}/home" class="animsition-link">Masuk Lagi</a>
