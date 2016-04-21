322fd30bc09b16ba11aeae37832e7ccc3af04446
awan21clouds <awan@21clouds.com>
2016-04-21 12:55:14 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -1,6 +1,24 @@
-Hello World!
 @extends('layout.master')
 
 @section('content')
-    Hello
+    <div class="container">
+        <!-- Main content -->
+        <section class="content">
+            <div class="row wow animated bounceIn" id="content"></div>
+
+            <div class="row wow animated bounceIn">
+                <div class="col-lg-12">
+                    <ul id="pagination" class="pagination"></ul>
+                </div>
+            </div>
+
+
+            @if(!Session::has('member'))
+                @include('layout.login')
+            @endif
+        </section><!-- /.content -->
+    </div><!-- /.container -->
+    <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <script src="../adminLTE/dist/js/home.js"></script>
+    @include('layout.script')
 @endsection
