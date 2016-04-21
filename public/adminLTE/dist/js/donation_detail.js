cd5e2641350fbb0ad3495e6840db97fb79fbade6
awan21clouds <awan@21clouds.com>
2016-04-21 13:41:11 +0700

    config

---
+++ public/adminLTE/dist/js/donation_detail.js
@@ -0,0 +1,6 @@
+/**
+ * Created by RizqyFahmi on 11/04/2016.
+ */
+$(document).ready(function(){
+    scholarshipRupiah();
+});
--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -29,6 +29,6 @@
         </section><!-- /.content -->
     </div><!-- /.container -->
     <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <script src="../adminLTE/dist/js/home.js"></script>
+    <script src="../adminLTE/dist/js/donation_detail.js"></script>
     @include('layout.script')
 @endsection
