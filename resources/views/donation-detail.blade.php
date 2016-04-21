90ecbcd2126bdb72d46b993d43414ca3c2068831
awan21clouds <awan@21clouds.com>
2016-04-21 13:17:08 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -1,23 +1,30 @@
 @extends('layout.master')
 
 @section('content')
-    <div class="lockscreen-wrapper animated rubberBand animsition">
-        <div class="lockscreen-logo">
-            <a href="javascript:;"><b>Terjadi Kesalahan!</b></a>
-        </div>
-        <!-- User name -->
-        <div class="lockscreen-name">Email dan password tidak valid!</div>
+    <div class="container">
+        <!-- Main content -->
+        <section class="content">
+            <div class="lockscreen-wrapper animated rubberBand animsition">
+                <div class="lockscreen-logo">
+                    <a href="javascript:;"><b>Terjadi Kesalahan!</b></a>
+                </div>
+                <!-- User name -->
+                <div class="lockscreen-name">Email dan password tidak valid!</div>
 
-        <div class="help-block text-center">
-            Pastikan anda memasukan email dan password yang valid!
-        </div>
-        <div class="text-center">
-            <a href="{{ url('/') }}/home" class="animsition-link">Masuk Lagi</a>
-        </div>
-        <div class="lockscreen-footer text-center">
-            Copyright &copy; 2016<br>
-        </div>
-    </div><!-- /.center -->
+                <div class="help-block text-center">
+                    Pastikan anda memasukan email dan password yang valid!
+                </div>
+                <div class="text-center">
+                    <a href="{{ url('/') }}/home" class="animsition-link">Masuk Lagi</a>
+                </div>
+                <div class="lockscreen-footer text-center">
+                    Copyright &copy; 2016<br>
+                </div>
+            </div><!-- /.center -->
+
+        </section><!-- /.content -->
+    </div><!-- /.container -->
     <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <script src="../adminLTE/dist/js/home.js"></script>
     @include('layout.script')
 @endsection
