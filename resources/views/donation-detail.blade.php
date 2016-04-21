e5bc69e5f55f5ef9b777a5f6118264c8a5c98d20
awan21clouds <awan@21clouds.com>
2016-04-21 13:06:54 +0700

    config

--- resources/views/donation-detail.blade.php
+++ resources/views/donation-detail.blade.php
@@ -9,8 +9,8 @@
                     <div class="box box-solid">
                         <div class="box-body">
                             <div class="callout callout-info">
-                                <h4>Tip!</h4>
-                                <p>Add the layout-top-nav class to the body tag to get this layout. This feature can also be used with a sidebar! So use this class if you want to remove the custom dropdown menus from the navbar and use regular links instead.</p>
+                                <h1>Donasi Berhasil!</h1>
+                                <p>Silahkan ke menu donasi untuk melihat detail dan konfirmasi pengiriman dana donasi</p>
                             </div>
                         </div><!-- /.box-body -->
                     </div><!-- /.box -->
