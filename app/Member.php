1b6c9dd3839e849992ae9096a7a1f85a8ea43913
awan21clouds <awan@21clouds.com>
2016-04-22 10:48:44 +0700

    config

--- app/Member.php
+++ app/Member.php
@@ -21,7 +21,7 @@ class Member extends Model
     public function login($email, $password){
         $member = DB::table('member')
 //            ->select('*', DB::raw('(DATE_FORMAT(member.regiter_date, "%T %d-%m-%Y")) as member_register_date'))
-            ->select('*')
+            ->select('*', DB::raw('(DATE_FORMAT(member.regiter_date, "%d-%m-%Y")) as member_register_date'))
             ->where('email', '=', $email)
             ->where('password', '=', $password)
             ->first();
