f91dfad49165639184797195f8494a099a95679b
awan21clouds <awan@21clouds.com>
2016-04-22 11:11:26 +0700

    config

--- resources/views/home-scholarship.blade.php
+++ resources/views/home-scholarship.blade.php
@@ -1,4 +1,9 @@
 <h2 class="page-header text-center">Beasiswa Sedang Dikumpulkan</h2>
 <div class="row wow animated bounceInUp">
     @include('layout.scholarship-template')
+
+</div>
+
+<div class="row wow animated bounceInUp text-center">
+    <a href="{{ url('/') }}/scholarship" class="btn btn-default">Selengkapnya</a>
 </div>
