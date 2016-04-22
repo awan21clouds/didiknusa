1278518dfe1049c87fa33321bd035aad5bc979ab
awan21clouds <awan@21clouds.com>
2016-04-22 15:52:08 +0700

    config

--- public/adminLTE/dist/js/donation.js
+++ public/adminLTE/dist/js/donation.js
@@ -52,7 +52,8 @@ function donation(){
     });
 
     var detailRows = [];
-    $('#dt-donation tbody').on( 'click', 'tr td.details-control, .details-control', function () {
+    //$('#dt-donation tbody').on( 'click', 'tr td.details-control, .details-control', function () {
+    $('#dt-donation tbody .details-control').on( 'click', function () {
         var tr = $(this).closest('tr');
         var row = dt_donation.row( tr );
         var idx = $.inArray( tr.attr('id'), detailRows );
