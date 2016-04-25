e2286347287c1e649d9617dd5a95b1b4123a40a2
awan21clouds <awan@21clouds.com>
2016-04-25 11:19:29 +0700

    config

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -53,8 +53,8 @@
         </div>
 
     </div>
-    <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-    <script src="../adminLTE/dist/js/home.js"></script>
-    @include('layout.script')
+    {{--<script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>--}}
+    {{--<script src="../adminLTE/dist/js/home.js"></script>--}}
+    {{--@include('layout.script')--}}
     </body>
 </html>
