4e4b1436e3f676af1ecadde90c3d438187b64080
awan21clouds <awan@21clouds.com>
2016-04-25 09:22:03 +0700

    config

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,26 +2,109 @@
     <head>
         <title>DidikNusa.com</title>
         @include('layout.style')
-    </head>
-    <body class="hold-transition lockscreen">
-        <div class="lockscreen-wrapper animated rubberBand animsition">
-            <div class="lockscreen-logo">
-                <a href="javascript:;"><b>Terjadi Kesalahan!</b></a>
-            </div>
-            <!-- User name -->
-            <div class="lockscreen-name">Email dan password tidak valid!</div>
+        <style>
+            * {
+                box-sizing: border-box;
+            }
 
-            <div class="help-block text-center">
-                Pastikan anda memasukan email dan password yang valid!
+            *:focus {
+                outline: none;
+            }
+            body {
+                font-family: Arial;
+                background-color: #3498DB;
+                padding: 50px;
+            }
+            .login {
+                margin: 20px auto;
+                width: 300px;
+            }
+            .login-screen {
+                background-color: #FFF;
+                padding: 20px;
+                border-radius: 5px
+            }
+
+            .app-title {
+                text-align: center;
+                color: #777;
+            }
+
+            .login-form {
+                text-align: center;
+            }
+            .control-group {
+                margin-bottom: 10px;
+            }
+
+            input {
+                text-align: center;
+                background-color: #ECF0F1;
+                border: 2px solid transparent;
+                border-radius: 3px;
+                font-size: 16px;
+                font-weight: 200;
+                padding: 10px 0;
+                width: 250px;
+                transition: border .5s;
+            }
+
+            input:focus {
+                border: 2px solid #3498DB;
+                box-shadow: none;
+            }
+
+            .btn {
+                border: 2px solid transparent;
+                background: #3498DB;
+                color: #ffffff;
+                font-size: 16px;
+                line-height: 25px;
+                padding: 10px 0;
+                text-decoration: none;
+                text-shadow: none;
+                border-radius: 3px;
+                box-shadow: none;
+                transition: 0.25s;
+                display: block;
+                width: 250px;
+                margin: 0 auto;
+            }
+
+            .btn:hover {
+                background-color: #2980B9;
+            }
+
+            .login-link {
+                font-size: 12px;
+                color: #444;
+                display: block;
+                margin-top: 12px;
+            }
+        </style>
+    </head>
+    <body>
+    <div class="login">
+        <div class="login-screen">
+            <div class="app-title">
+                <h1>Login</h1>
             </div>
-            <div class="text-center">
-                <a href="{{ url('/') }}/home" class="animsition-link">Masuk Lagi</a>
+
+            <div class="login-form">
+                <div class="control-group">
+                    <input type="text" class="login-field" value="" placeholder="username" id="login-name">
+                    <label class="login-field-icon fui-user" for="login-name"></label>
+                </div>
+
+                <div class="control-group">
+                    <input type="password" class="login-field" value="" placeholder="password" id="login-pass">
+                    <label class="login-field-icon fui-lock" for="login-pass"></label>
+                </div>
+
+                <a class="btn btn-primary btn-large btn-block" href="#">login</a>
+                <a class="login-link" href="#">Lost your password?</a>
             </div>
-            <div class="lockscreen-footer text-center">
-                Copyright &copy; 2016<br>
-            </div>
-        </div><!-- /.center -->
-        <script src="../adminLTE/plugins/jQuery/jQuery-2.1.4.min.js"></script>
-        @include('layout.script')
+        </div>
+    </div>
     </body>
 </html>
